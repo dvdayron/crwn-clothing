e919c36958995a191eace5613256355e0d7bc04e
Dayron <dvdayron@gmail.com>
2021-01-22 22:14:15 -0500

    sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {singInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, singInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props){
@@ -13,12 +13,20 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async e => {
     e.preventDefault(); 
-    this.setState({
-      email: '',
-      password: ''
-    });
+    const {email, password} = this.state;
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: ''
+      });
+    } catch(error) {
+      console.error(error);
+    }
+
+    
   } 
 
   handleChange = (e) => {
